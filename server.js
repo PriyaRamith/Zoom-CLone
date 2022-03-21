132d8edf03292122f9e279e0afe5f15a1e3869ba
MPLGROUP\priyanka.s <priyanka.s@manipalgroup.info>
2022-03-21 12:05:10 +0530

    Server port listen update

--- server.js
+++ server.js
@@ -34,4 +34,7 @@ io.on('connection', socket => {
       })
     })
   })
-server.listen(3000)
+// server.listen(3000)
+server.listen(process.env.PORT || 3000,'0.0.0.0', () => {
+  console.log("Listening on port 3000...")
+})
