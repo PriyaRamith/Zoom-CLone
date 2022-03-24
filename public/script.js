d6c57ce23da2134816dfc0b05050d85d4dc8bfd4
MPLGROUP\priyanka.s <priyanka.s@manipalgroup.info>
2022-03-24 11:48:51 +0530

    updated

--- public/script.js
+++ public/script.js
@@ -6,35 +6,41 @@ const playButton = document.querySelector('button#play');
 const downloadButton = document.querySelector('button#download');
 const recordedVideo = document.querySelector('video#recorded');
 const uploadVideo = document.querySelector('button#upload');
-const myPeer = new Peer(undefined, {
-  host: 'peerjs-server.herokuapp.com',
-  secure: true,
-  port: 443,
-  config: {'iceServers': [
-    { url: 'stun:stun.l.google.com:19302' },
-    {
-      urls: "stun:openrelay.metered.ca:80"
-    },
-    {
-      urls: "turn:openrelay.metered.ca:80",
-      username: "openrelayproject",
-      credential: "openrelayproject"
-    },
-    {
-      urls: "turn:openrelay.metered.ca:443",
-      username: "openrelayproject",
-      credential: "openrelayproject"
-    },
-    {
-      urls: "turn:openrelay.metered.ca:443?transport=tcp",
-      username: "openrelayproject",
-      credential: "openrelayproject"
-    }
+// const myPeer = new Peer(undefined, {
+//   host: 'peerjs-server.herokuapp.com',
+//   secure: true,
+//   port: 443,
+//   config: {'iceServers': [
+//     { url: 'stun:stun.l.google.com:19302' },
+//     {
+//       urls: "stun:openrelay.metered.ca:80"
+//     },
+//     {
+//       urls: "turn:openrelay.metered.ca:80",
+//       username: "openrelayproject",
+//       credential: "openrelayproject"
+//     },
+//     {
+//       urls: "turn:openrelay.metered.ca:443",
+//       username: "openrelayproject",
+//       credential: "openrelayproject"
+//     },
+//     {
+//       urls: "turn:openrelay.metered.ca:443?transport=tcp",
+//       username: "openrelayproject",
+//       credential: "openrelayproject"
+//     }
     
 
-  ]} /* Sample servers, please use appropriate ones */
+//   ]} /* Sample servers, please use appropriate ones */
+// })
+const myPeer = new Peer(undefined, {
+  path: '/peerjs',
+  host: '/',
+  port: '443'
 })
 var  chunks=[] ;
+let myVideoStream;
 const myVideo = document.createElement('video')
 myVideo.muted = true
 const peers = {}
@@ -44,6 +50,7 @@ navigator.mediaDevices.getUserMedia({
   
    
 }).then(stream => {
+  myVideoStream = stream;
   addVideoStream(myVideo, stream)
   
   var mediaRecorder = new MediaRecorder(stream);
@@ -147,7 +154,19 @@ navigator.mediaDevices.getUserMedia({
     connectToNewUser(userId, stream)
   })
 })
-
+ // input value
+ let text = $("input");
+ // when press enter send message
+ $('html').keydown(function (e) {
+   if (e.which == 13 && text.val().length !== 0) {
+     socket.emit('message', text.val());
+     text.val('')
+   }
+ });
+ socket.on("createMessage", message => {
+   $("ul").append(`<li class="message"><b>user</b><br/>${message}</li>`);
+   scrollToBottom()
+ })
 socket.on('user-disconnected', userId => {
   if (peers[userId]) peers[userId].close()
 })
@@ -242,3 +261,77 @@ console.log(recordedVideo.src)
 //       request.send(data);
 //   }
 // });
+myPeer.on('open', id => {
+  socket.emit('join-room', ROOM_ID, id)
+})
+function connectToNewUser(userId, stream) {
+  const call = myPeer.call(userId, stream)
+  const video = document.createElement('video')
+  call.on('stream', userVideoStream => {
+    addVideoStream(video, userVideoStream)
+  })
+  call.on('close', () => {
+    video.remove()
+  })
+  peers[userId] = call
+}
+function addVideoStream(video, stream) {
+  video.srcObject = stream
+  video.addEventListener('loadedmetadata', () => {
+    video.play()
+  })
+  videoGrid.append(video)
+}
+const scrollToBottom = () => {
+  var d = $('.main__chat_window');
+  d.scrollTop(d.prop("scrollHeight"));
+}
+const muteUnmute = () => {
+  const enabled = myVideoStream.getAudioTracks()[0].enabled;
+  if (enabled) {
+    myVideoStream.getAudioTracks()[0].enabled = false;
+    setUnmuteButton();
+  } else {
+    setMuteButton();
+    myVideoStream.getAudioTracks()[0].enabled = true;
+  }
+}
+const playStop = () => {
+  console.log('object')
+  let enabled = myVideoStream.getVideoTracks()[0].enabled;
+  if (enabled) {
+    myVideoStream.getVideoTracks()[0].enabled = false;
+    setPlayVideo()
+  } else {
+    setStopVideo()
+    myVideoStream.getVideoTracks()[0].enabled = true;
+  }
+}
+const setMuteButton = () => {
+  const html = `
+    <i class="fas fa-microphone"></i>
+    <span>Mute</span>
+  `
+  document.querySelector('.main__mute_button').innerHTML = html;
+}
+const setUnmuteButton = () => {
+  const html = `
+    <i class="unmute fas fa-microphone-slash"></i>
+    <span>Unmute</span>
+  `
+  document.querySelector('.main__mute_button').innerHTML = html;
+}
+const setStopVideo = () => {
+  const html = `
+    <i class="fas fa-video"></i>
+    <span>Stop Video</span>
+  `
+  document.querySelector('.main__video_button').innerHTML = html;
+}
+const setPlayVideo = () => {
+  const html = `
+  <i class="stop fas fa-video-slash"></i>
+    <span>Play Video</span>
+  `
+  document.querySelector('.main__video_button').innerHTML = html;
+}
